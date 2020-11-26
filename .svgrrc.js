724255cc8b942f404bea0cc84e266ed5c38e9173
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-11-26 11:18:41 +0100

    Updated config files

---
+++ .svgrrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  icon: false,
+  native: true,
+  typescript: true,
+  dimensions: false,
+  expandProps: 'end',
+  ref: true,
+  memo: true,
+  svgProps: {
+    width: 46,
+    height: 46,
+    fill: '#000',
+  },
+};
